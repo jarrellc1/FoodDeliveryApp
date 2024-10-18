2361cdb87628013b5e4e31e831fcfc4ed8e40406
jarrellc1 <Jarrell.chia@gmail.com>
2024-10-19 01:29:51 +0800

    stop at 9:09

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:fooddeliveryapp/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.lock_open_rounded,
+            size: 100,
+            color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+
+            const SizedBox(height: 25),
+
+            Text(
+              "Food Delivery App", 
+            style: TextStyle(
+              fontSize: 16,
+              color: Theme.of(context).colorScheme.inversePrimary,
+               )
+            ),
+
+            const SizedBox(height: 25),
+
+            MyTextField(
+              controller : emailController,
+              hintText : "Email",
+              obscureText: false,
+            ),
+
+            MyTextField(
+              controller: passwordController, 
+              hintText: "Password", 
+              obscureText: true
+            )
+
+        ],
+        )
+
+      ),
+    );
   }
 }
